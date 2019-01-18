e3fd9f2579b7baed0353c5316cfc3bfab0f3da65
Andres Contreras <andrescontreras@Andress-MacBook-Air-2.local>
2019-01-17 23:50:10 -0800

    Updated app.js to have madeup data

--- src/App.js
+++ src/App.js
@@ -7,16 +7,49 @@ import './App.css';
 //let defaultTextColor_Green = '#1db954';
 
 //Creating a default style object
-//So now we dont need to style with style = {{}} but just style = {} since this is an object
+//So now we dont need to style with style = {{}} but just style = {} since this is an object Data
 let defaultStyle = {
   color : '#1db954'
 };
+
+
+// Comming up with madeup data
+let fakeServerData = {
+  user : {
+    name : 'Andy',
+    playlists : [
+      {
+        name : 'Wanderlust',
+        songs: ['Wanderlust','Rocketman (feat. Atlas & Lando!)']
+      },
+
+      {
+        name : 'Crescendo',
+        songs: ['Sunroof','Bout it (feat. Daniel James & Benjamin O)', 'Kid Again']
+
+      },
+
+      {
+        name : 'Lock My Door',
+        songs: ['The Glo','Polaroids', 'throwback (feat. Saba)'] 
+      },
+
+      { 
+        name : 'Best For You',
+        songs: ['Coming back','Stay focus', 'Waves']
+      }
+    ]
+  }
+
+  };
 
 /*  		○ Aggregation Data [Component_B] ○
 * Available as a Tag
 * Divs are block by default. 'inline-block' will help us make it look more smooth 
 */
 class Aggregate extends Component {
+  //Playlist will be available and passed in this component as 'props' so instead of calling
+  // this.state.serverData.user.playlists.lengh we narrow it down to this.props.playlists
   render() {
     return (
 
@@ -27,7 +60,7 @@ class Aggregate extends Component {
       */
       <div style = {{...defaultStyle, width: '40%', display: 'inline-block'}}>
         <h2>
-          Number Text
+          {this.props.playlists && this.props.playlists.length} Text
         </h2>
       </div>
       //This returns some HTML with some styling
@@ -79,6 +112,18 @@ class Playlist extends Component {
 
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {serverData: {}}
+  }
+  componentDidMount() {
+    //After the five seconds, it will execute the function that we passed in
+    setTimeout(() =>{
+    this.setState({serverData: fakeServerData});
+  }, 2000);
+}
+
   render() {
       /*
       * Two ways of calling Tag
@@ -86,15 +131,22 @@ class App extends Component {
       */
     return (
       <div className="App">
-        <h1 style = {{...defaultStyle, 'font-size': '54px', 'color' : '#1db954'}}>Title</h1>
-        <Aggregate/>
-        <Aggregate/>
-        <Filter/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
-      </div>
+        {this.state.serverData.user ?
+        <div>
+          <h1 style = {{...defaultStyle, 'font-size': '54px', 'color' : '#1db954'}}>
+            {this.state.serverData.user.name}'s Playlist 
+          </h1>
+
+          <Aggregate playlists = {this.state.serverData.user.playlists}/>
+          <Aggregate/>
+          <Filter/>
+          <Playlist/>
+          <Playlist/>
+          <Playlist/>
+          <Playlist/>
+        </div> :  <h1 style = {defaultStyle} > Loading... </h1>
+        }
+        </div>
     );
   }
 }
